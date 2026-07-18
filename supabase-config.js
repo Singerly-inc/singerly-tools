95bd16d4b52ec538fca448476bf9a9b06198b811
Masahiro Okuda <masahiro.okuda@singerly.co.jp>
2026-07-19 08:12:56 +0900

    fix: 正しいpublishableキーに差し替え

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
 // Supabase設定
 const SUPABASE_URL = 'https://snkoiisiaevhknajirol.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_kmkF5bh0ab6I-bHtC6LJvw_HM6dGUxM';
+const SUPABASE_ANON_KEY = 'sb_publishable_nBeSh8gtJll-vrrLT2DN8A_-IR-kFsp';
